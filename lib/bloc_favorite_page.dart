a31768c7d3a4e174a2025c16980b8e92fef61bc6
canisterism <ohtthstc@gmail.com>
2019-12-20 20:01:49 +0900

    ✨bloc_favorite_page

---
+++ lib/bloc_favorite_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:bloc_tutorial/models/word_item.dart';
+import 'package:bloc_tutorial/word_provider.dart';
+
+class BlockFavoritePage extends StatelessWidget {
+  BlockFavoritePage();
+
+  static const routeName = "/favorite";
+  @override
+  Widget build(BuildContext context) {
+    final word = WordProvider.of(context);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Your Favorite"),
+        ),
+        body: StreamBuilder<List<WordItem>>(
+          stream: word.items,
+          builder: (context, snapShot) {
+            if (snapShot.data == null || snapShot.data.isEmpty) {
+              return Center(child: Text("Empty"));
+            }
+            final tiles = snapShot.data.map((item) {
+              return ListTile(
+                title: new Text(item.name),
+              );
+            });
+            final divided =
+                ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+            return new ListView(children: divided);
+          },
+        ));
+  }
+}
